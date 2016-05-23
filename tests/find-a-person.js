76cc9866a8973fc71104f895681950ef1750d050
orayr <orajerusalem1995@gmail.com>
2016-05-23 16:12:42 +0300

    GREEN: 2 test for find a person -passed

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -1,11 +1,18 @@
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
-//my change
+
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+     	
+  });
+it('Given a missing person name, return empty posts list', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var posts = map.find_a_person("Ora")
+    expect(posts).to.be.eql([]);
+     	
   });
 });
